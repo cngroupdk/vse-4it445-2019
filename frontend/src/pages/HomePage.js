c17157718a430c5f5d94bcee37c4847843278c51
Tomas Horacek <tomas.horacek@gmail.com>
2019-09-17 15:52:11 +0000

    Practical 01 16:15 end

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,12 +3,27 @@ import React from 'react';
 import { Heading, MainSection } from '../atoms/';
 import { TopNavigation } from '../organisms/TopNavigation';
 
+function Quack({ text, user = 'unknown' }) {
+  return (
+    <div>Quack: {text} (@{user})</div>
+  )
+}
+
 export function HomePage() {
   return (
     <div>
       <TopNavigation />
       <MainSection>
-        <Heading>Hello, 4IT445!</Heading>
+        <Heading>Hello, World!</Heading>
+
+        <Quack text="123" user="johndoe" />
+        <Quack text="Hello, 4IT445!"/>
+        <Quack text="9999999"/>
+
+        <h2>123 hello!</h2>
+        <div>this is JSX</div>
+        <p>this is XML in JS</p>
+        <input type="text" />
       </MainSection>
     </div>
   );
