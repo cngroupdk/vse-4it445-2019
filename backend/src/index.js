d6735100a56a970edff14affbdc5bb183de58f14
Petr Čaněk <canekpet@gmail.com>
2019-10-16 10:57:09 +0200

    Basic back-end implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,40 @@
 import dotenv from 'dotenv';
+import express from 'express';
+
+import { quacks, users } from './mocks';
 
 dotenv.config();
 dotenv.config({ path: '.env.local' });
 
+const app = express();
+
 const { PORT = 3001 } = process.env;
+
+app.use('/quack/:quackId', (req, res, next) => {
+  const { quackId } = req.params;
+  const quack = quacks.find(item => Number(item.id) === Number(quackId));
+
+  if (!quack) {
+    res.status(404);
+    return res.json({ error: true, msg: 'No quacks for you!' });
+  }
+
+  res.json(quack);
+});
+
+app.use('/quack', (req, res, next) => {
+  res.json(quacks);
+});
+
+app.use('/user', (req, res, next) => {
+  res.json(users);
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ error: '404: Not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}!`);
+});
