99cee18ee48d8eff134aa35fafedb2f4f4e10cbe
Petr Čaněk <canekpet@gmail.com>
2019-10-22 17:13:32 +0200

    changed back-end to use DB

--- backend/src/modules/quack/quackRoutes.js
+++ backend/src/modules/quack/quackRoutes.js
@@ -1,12 +1,43 @@
 import { Router } from 'express';
 
-import { quacks } from './mocks';
+import { DB_CONNECTION_KEY } from '../../libs/connection';
 
 const router = Router();
 
-router.use('/:quackId', (req, res, next) => {
+router.use('/addNew', async (req, res, next) => {
+  const dbConnection = req[DB_CONNECTION_KEY];
+  const data = req.body;
+  const { name, screenName, profileImageUrl } = data;
+  console.log('incoming data', data);
+
+  const dbResponse = await dbConnection.query(
+    `INSERT INTO quack (id, name, screenName, profileImageUrl) 
+    VALUES (NULL, '${name}', '${screenName}', '${profileImageUrl}');`,
+  );
+
+  console.log('DB response', dbResponse);
+
+  res.status(404);
+  res.json({ error: true, msg: 'Něco' });
+  return;
+});
+
+router.use('/:quackId', async (req, res, next) => {
   const { quackId } = req.params;
-  const quack = quacks.find(item => Number(item.id) === Number(quackId));
+  const quackIdNumber = Number(quackId);
+
+  if (!quackIdNumber) {
+    res.status(404);
+    res.json({ error: true, msg: 'Wrong input!' });
+    return;
+  }
+
+  const dbConnection = req[DB_CONNECTION_KEY];
+  const quackFromDb = await dbConnection.query(
+    `SELECT * FROM quack WHERE id = ${quackId}`,
+  );
+
+  const quack = quackFromDb[0];
 
   if (!quack) {
     res.status(404);
@@ -16,8 +47,12 @@ router.use('/:quackId', (req, res, next) => {
   res.json(quack);
 });
 
-router.use('/', (req, res, next) => {
-  res.json(quacks);
+router.use('/', async (req, res, next) => {
+  const dbConnection = req[DB_CONNECTION_KEY];
+  const quackFromDb = await dbConnection.query('SELECT * FROM `quack` WHERE 1');
+
+  console.log('test', quackFromDb);
+  res.json(quackFromDb);
 });
 
 export default router;
